b26ae9e3f699d0e57db170a4fe388c90fa5cb7d3
Locklear66 <111928330+Locklear66@users.noreply.github.com>
2023-01-20 18:09:22 -0600

    finished

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 // template_bpavk3e
 // service_uc6u3wm
 // rJeubM889V0KM5C2W
+const scaleFactor = 1/20
 
-function contact(event) {
-   event.preventDefault();
-   emailjs
-      .sendForm(
-         'template_bpavk3e',
-         'service_uc6u3wm',
-         event.target,
-         'rJeubM889V0KM5C2W'
-   ).then(() => {
-      console.log('this worked')
-   })
-   // setTimeout(() => {
-   //    console.log('it worked 1')
-   // }, 500);
-   
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
 }
 
-console.log()
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += 'dark-theme'
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+
+
+function contact(event){
+  event.preventDefault();
+  const loading = document.querySelector('.model__overlay--loading');
+  const success = document.querySelector('.model__overlay--success');
+  loading.classList += " model__overlay--visible"
+  emailjs
+     .sendForm(
+        'service_uc6u3wm',
+        'template_bpavk3e',
+        event.target,
+        'rJeubM889V0KM5C2W' 
+
+  ).then(() => {
+      loading.classList.remove("model__overlay--visible");
+      success.classList += " model__overlay--visible";
+     }).catch(() => {
+       loading.classList.remove("model__overlay--visible");
+       alert(
+         "The email service is temporarily down. Please contact me at samuel.wooley@live.com"
+       )
+     })
+}
+
+let isModelOpen = false;
+function toggleModel() {
+  if (isModelOpen) {
+    isModelOpen = false
+    return document.body.classList.remove("model--open")
+  }
+  isModelOpen = true;
+  document.body.classList += " model--open";
+}
